1802a729a6a086ef3da83702071b5255ec177613
MarkSelden <seldenm@berkeley.edu>
2020-12-07 13:36:45 -0800

    pushing for debugging help

--- solver.py
+++ solver.py
@@ -48,36 +48,90 @@ class GreedySolver():
         for student in range(nx.number_of_nodes(G)):
             self.rooms.append([student])
 
-    def merge(self, r1, r2):
-        self.rooms[r1] += self.rooms[r2]
-        self.rooms.pop(r2)
+    def merge(self, roomies):
+        print("pre merge: {}".format(self.rooms))
+        print("merging: {}".format(roomies))
 
+        main_room = roomies[0]
+        for i in range(1, len(roomies)):
+            self.rooms[main_room] += self.rooms[roomies[i]]
+
+        for i in reversed(range(1, len(roomies))):
+            self.rooms.pop(roomies[i])
+
+        print("merged: {}".format(self.rooms))
+
+    #add memoization maybe
     #passes two rooms in, returns the value of merging them or -1 if merge would be invalid
-    def check_merge(self, r1, r2):
-       new_room = r1 + r2
-       if calculate_stress_for_room(new_room, self.G) <= (self.s/len(self.rooms) - 1):
-           happy_gain = calculate_happiness_for_room(new_room, self.G) - calculate_happiness_for_room(r1, self.G) - calculate_happiness_for_room(r2, self.G)
-           stress_gain = calculate_stress_for_room(new_room, self.G) - calculate_stress_for_room(r1, self.G) - calculate_stress_for_room(r2, self.G)
+    def check_merge(self, roomies):
+       new_room = []
+       for r in roomies:
+           new_room += r
+
+       if calculate_stress_for_room(new_room, self.G) < (self.s/(len(self.rooms) - len(roomies) + 1)):
+           happy_gain = calculate_happiness_for_room(new_room, self.G)
+
+           for r in roomies:
+               happy_gain -= calculate_happiness_for_room(r, self.G)
+           stress_gain = calculate_stress_for_room(new_room, self.G)
+
+           for r in roomies:
+               stress_gain -= calculate_stress_for_room(r, self.G)
+
            return ((happy_gain * happy_gain) / max(stress_gain, 1))
        else:
            return -1
 
+
     def solve(self):
         while True:
-            r1, r2, val = self.find_merge()
+
+            print(self.rooms)
+            roomies, val = self.find_merge()
             if val <= 0:
                 return
-            self.merge(r1, r2)
+            self.merge(roomies)
+
 
     #finds the best rooms to merge according to the value function returened by check_merge
     def find_merge(self):
-        best = (0, 0, 0)
-        #iterates through all of the combos of rooms
+        best = ([], 0)
+        #iterates through all of the combos of rooms within 4.
+        merge = []
         for i in range(len(self.rooms)):
+            merge.append(self.rooms[i])
+
             for j in range(i + 1, len(self.rooms)):
-                val = self.check_merge(self.rooms[i], self.rooms[j])
-                if val > best[2]:
-                    best = (i, j, val)
+                merge.append(self.rooms[j])
+                val = self.check_merge(merge)
+                if val > best[1]:
+
+                    best = ([i, j], val)
+
+
+                for k in range(j + 1, len(self.rooms)):
+                    merge.append(self.rooms[k])
+                    val = self.check_merge(merge)
+                    if val > best[1]:
+
+                        best = ([i, j, k], val)
+
+
+                    for l in range(k + 1, len(self.rooms)):
+                        merge.append(self.rooms[k])
+                        val = self.check_merge(merge)
+                        if val > best[1]:
+
+                            best = ([i, j, k, l], val)
+                        #pop the values from the merge array once they have been checked.
+                        merge.pop()
+
+                    merge.pop()
+
+                merge.pop()
+
+            merge.pop()
+        print("best merge: {} ".format(best))
         return best
 
 class BranchAndBoundSolver():
@@ -144,13 +198,18 @@ class BranchAndBoundSolver():
 
 # Usage: python3 solver.py test.in
 
-#if __name__ == '__main__':
- #   assert len(sys.argv) == 2
- #   path = sys.argv[1]
- #   G, s = read_input_file(path)
- #   solve(G, s)
-    #assert is_valid_solution(D, G, s, k)
-    #print("Total Happiness: {}".format(calculate_happiness(D, G)))
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G, s = read_input_file(path)
+    cool = GreedySolver(G, s)
+    cool.solve()
+    solution = cool.rooms
+    rooms = len(solution)
+    D, k = convert_dictionary(convert_list(solution)), rooms
+    assert is_valid_solution(D, G, s, k)
+    happiness = calculate_happiness(D, G)
+    print("Total Happiness: {}".format(calculate_happiness(D, G)))
 
 
 #For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
