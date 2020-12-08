8662f50a153d0a6c9c937d9f0da144a491cd98dc
MarkSelden <seldenm@berkeley.edu>
2020-12-07 23:04:38 -0800

    lots of misc folders but done

--- greedy_solution_generator.py
+++ greedy_solution_generator.py
@@ -5,21 +5,21 @@ import sys
 from os.path import basename, normpath
 import glob
 import copy
-from solver import GreedySolver, convert_list
+from solver import GreedySolver, convert_list, BranchAndBoundSolver
 import multiprocessing as mp
 
 def output_path(input_path):
-    return 'outputs/large/' + basename(normpath(input_path))[:-3] + '.out'
+    return 'medium/' + basename(normpath(input_path))[:-3] + '.out'
 
 
 def solve(input_path):
 
     G, s = read_input_file(input_path)
-    cool = GreedySolver(G, s)
+    cool = BranchAndBoundSolver(G, s)
     cool.solve()
-    solution = cool.rooms
+    solution = cool.best
     rooms = len(solution)
-    D, k = convert_dictionary(convert_list(solution)), rooms
+    D, k = convert_dictionary(solution), rooms
     assert is_valid_solution(D, G, s, k)
     happiness = calculate_happiness(D, G)
     print("{} Total Happiness: {}".format(output_path(input_path), happiness))
@@ -27,7 +27,7 @@ def solve(input_path):
 
 
 if __name__ == '__main__':
-    inputs = glob.glob('inputs/large/*')
+    inputs = glob.glob('inputs/medium/*')
     p = mp.Pool(mp.cpu_count())
     p.map(solve, [input_path for input_path in inputs])
 
